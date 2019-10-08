1f0c9d854e44530e47da6cc3b22f785e59238740
grepto <grepto@gmail.com>
2019-10-08 10:33:59 +0300

    Optimize popular posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.db.models import Count
+
 from blog.models import Comment, Post, Tag
 
 
@@ -28,7 +30,7 @@ def serialize_tag(tag):
 
 
 def index(request):
-    most_popular_posts = sorted(Post.objects.all(), key=get_likes_count)[-5:]
+    most_popular_posts = sorted(Post.objects.annotate(Count('likes')), key=get_likes_count)[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -113,4 +115,4 @@ def contacts(request):
 
 
 def get_likes_count(post):
-    return post.likes.count()
+    return post.likes__count
